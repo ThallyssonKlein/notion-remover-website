72896e1fc1ae2421d1fd44286817b10b60aba51b
thallyssonklein <thallyssonklein@gmail.com>
2025-01-18 17:51:42 -0300

    fix: page title

--- notion-remover-website/pages/_document.tsx
+++ notion-remover-website/pages/_document.tsx
@@ -6,6 +6,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="icon" href="/notion-logo.png" />
+          <title>Notion Remover</title>
         </Head>
         <body>
           <Main />
